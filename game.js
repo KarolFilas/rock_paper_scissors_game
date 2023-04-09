0e97857e751f725b5391c63ce0049481fd7d9620
Karol Filas <filaskarol@gmail.com>
2023-04-09 20:46:27 +0200

    publication of the game result

--- game.js
+++ game.js
@@ -25,18 +25,26 @@ function aiChoice() {
     return aiHand
 }
 
+function checkResult(player, ai) {
+    if (player === ai) {
+        return 'draw'
+    } else if ((player === 'papier' && ai === 'kamień') ||
+        (player === 'kamień' && ai === 'nożyczki') ||
+        (player === 'nożyczki' && ai === 'papier')) {
+        return 'win'
+    } else {
+        return 'loss'
+    }
+}
+
 // funkcja sterująca
 function startGame() {
     if (!game.playerHand) {
         return alert('Wybierz dłoń')
     }
-
     game.aiHand = aiChoice()
-
+    const gameResult = checkResult(game.playerHand, game.aiHand)
 }
-
-
-
 
 hands.forEach(hand => hand.addEventListener('click', handSelection))
 
