e577863d8e18457ecc96fd6efb8429bc87da9888
Karol Filas <filaskarol@gmail.com>
2023-04-09 17:45:17 +0200

    choosing  player's hand

--- game.js
+++ game.js
@@ -1,1 +1,47 @@
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+}
+
+const game = {
+    playerHand: '',
+    aiHand: '',
+}
+
+const hands = [...document.querySelectorAll('.select img')]
+
+// pierwsza funkcja
+function handSelection() {
+    console.log(this);
+    game.playerHand = this.dataset.option
+    hands.forEach(hand => hand.style.boxShadow = '')
+    this.style.boxShadow = '0 0 0 4px red'
+}
+
+
+
+hands.forEach(hand => hand.addEventListener('click', handSelection))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
